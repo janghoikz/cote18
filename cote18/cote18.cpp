6aef5811eca4bf5b432460b97ebff582fdf11469
ohne <ghdlr785@gmail.com>
2022-06-25 18:42:25 +0900

    초기 커밋.

---
+++ cote18/cote18.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    vector<bool> chk(n + 1, 0);
+
+    for (int i = 2; i <= n; i++) 
+    {
+        if (chk[i])
+        {
+            continue;
+        }
+        for (int j = i + i; j <= n; j += i)
+        {
+            chk[j] = 1;
+        }
+    }
+
+    for (int i = 2; i <= n; i++) 
+    {
+        if (!chk[i]) 
+        {
+            answer++;
+        }
+    }
+
+    return answer;
+}
+
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
